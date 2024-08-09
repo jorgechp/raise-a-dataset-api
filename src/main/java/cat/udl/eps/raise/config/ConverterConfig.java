b0f33e6b0909b3c61c89acda6d21ffb4279b93d7
jorgechp <jorge.chamorro@udl.cat>
2024-08-09 12:17:58 +0200

    Remove ConverterFAIRPrincipleVerificationInstanceDTO.java

--- src/main/java/cat/udl/eps/raise/config/ConverterConfig.java
+++ src/main/java/cat/udl/eps/raise/config/ConverterConfig.java
@@ -1,27 +1,10 @@
-package cat.udl.eps.raise.converter;
-
-
-import cat.udl.eps.raise.domain.Verification;
-import cat.udl.eps.raise.projection.VerificationDTO;
-import org.springframework.context.annotation.Bean;
+package cat.udl.eps.raise.config;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.format.support.DefaultFormattingConversionService;
+
 
 @Configuration
 public class ConverterConfig {
 
-    private final Converter<Verification, VerificationDTO> tupleBackedMapToDtoConverter;
 
-    public ConverterConfig(Converter<Verification, VerificationDTO> tupleBackedMapToDtoConverter) {
-        this.tupleBackedMapToDtoConverter = tupleBackedMapToDtoConverter;
-    }
-
-    @Bean
-    public DefaultFormattingConversionService conversionService() {
-        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
-        conversionService.addConverter(tupleBackedMapToDtoConverter);
-        return conversionService;
-    }
 }
 
