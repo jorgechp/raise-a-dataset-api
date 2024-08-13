d6f7dd130a80cf9628c5acec87f589a6c0c5bfdd
jorgechp <jorge.chamorro@udl.cat>
2024-08-13 12:36:32 +0200

    Implement retrieveComplianceDTONotEvaluatedByUser and retrieveComplianceDTONotEvaluated

--- src/main/java/cat/udl/eps/raise/repository/ComplianceRepository.java
+++ src/main/java/cat/udl/eps/raise/repository/ComplianceRepository.java
@@ -36,4 +36,14 @@ public interface ComplianceRepository extends
             " p.instance.repository.name, p.instance.dataset.id, p.instance.dataset.name, p.principle.namePrefix, p.principle.name, " +
             " p.principle.category, p.verificationDate) FROM Compliance p")
     ComplianceDTO[] retrieveAllComplianceDTO();
+
+    @Query("SELECT new cat.udl.eps.raise.projection.ComplianceDTO(p.id, p.principle.id, p.author.id, p.author.username, p.instance.id, p.instance.repository.id," +
+            "    p.instance.repository.name, p.instance.dataset.id, p.instance.dataset.name, p.principle.namePrefix, p.principle.name," +
+            "    p.principle.category, p.verificationDate) FROM Compliance p LEFT JOIN Validation v ON p.id = v.compliance.id AND v.validator.id = :userId WHERE v.id is NULL")
+    ComplianceDTO[] retrieveComplianceDTONotEvaluatedByUser(@Param("userId") Long userId);
+
+    @Query("SELECT new cat.udl.eps.raise.projection.ComplianceDTO(p.id, p.principle.id, p.author.id, p.author.username, p.instance.id, p.instance.repository.id," +
+            "    p.instance.repository.name, p.instance.dataset.id, p.instance.dataset.name, p.principle.namePrefix, p.principle.name," +
+            "    p.principle.category, p.verificationDate) FROM Compliance p LEFT JOIN Validation v ON p.id = v.compliance.id WHERE v.id is NULL")
+    ComplianceDTO[] retrieveComplianceDTONotEvaluated();
 }
