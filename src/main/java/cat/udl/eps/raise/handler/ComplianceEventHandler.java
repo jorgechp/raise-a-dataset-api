59633372087161335c380595ae063b31cf3fbaaf
jorgechp <jorge.chamorro@udl.cat>
2024-08-29 23:37:05 +0200

    Define handleAuthorAfterCreate for ComplianceEventHandler

--- src/main/java/cat/udl/eps/raise/handler/ComplianceEventHandler.java
+++ src/main/java/cat/udl/eps/raise/handler/ComplianceEventHandler.java
@@ -2,9 +2,14 @@ package cat.udl.eps.raise.handler;
 
 
 import cat.udl.eps.raise.domain.Compliance;
+import cat.udl.eps.raise.domain.RaiseInstance;
 import cat.udl.eps.raise.repository.ComplianceRepository;
+import cat.udl.eps.raise.repository.RaiseInstanceRepository;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @org.springframework.data.rest.core.annotation.RepositoryEventHandler
@@ -12,10 +17,12 @@ public class ComplianceEventHandler {
 
 
     final ComplianceRepository complianceRepository;
+    final RaiseInstanceRepository raiseInstanceRepository;
 
     public ComplianceEventHandler(
-            ComplianceRepository complianceRepository) {
+            ComplianceRepository complianceRepository, RaiseInstanceRepository raiseInstanceRepository) {
         this.complianceRepository = complianceRepository;
+        this.raiseInstanceRepository = raiseInstanceRepository;
     }
 
     @HandleBeforeCreate
@@ -27,6 +34,15 @@ public class ComplianceEventHandler {
         if(response){
             throw new RuntimeException("Another similar instance exists");
         }
+    }
+
+    @HandleAfterCreate
+    public void handleAuthorAfterCreate(Compliance compliance){
+        RaiseInstance ri = compliance.getInstance();
+        short daysToAdd = ri.getFeedFrequencyInDays();
+        LocalDate ld = LocalDate.now().plusDays(daysToAdd);
+        ri.setNextFeedAction(ld);
+        raiseInstanceRepository.save(ri);
     }
 
 }
